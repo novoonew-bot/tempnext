ff4e0e7e90cc7e2244780d4cc26c244a66165429
Josemar <novoonew@gmail.com>
2026-06-15 22:05:04 -0400

    v429 home reorder card lugar abaixo da grade foto grande sobe

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v428
-const SW_VERSION = "v428";
+// Tempnext Service Worker v429
+const SW_VERSION = "v429";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
