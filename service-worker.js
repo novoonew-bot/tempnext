01f1bcb68fdc44588a6f1c7b840a70c10c7045f1
Josemar <novoonew@gmail.com>
2026-06-16 22:10:16 -0400

    v432 cadastro jornada guiada passo a passo marca nova local de nascimento

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v431
-const SW_VERSION = "v431";
+// Tempnext Service Worker v432
+const SW_VERSION = "v432";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
