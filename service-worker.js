ac22322b6a0605fa2a22ab92af90f5e70de74f3e
Josemar <novoonew@gmail.com>
2026-06-18 19:09:13 -0400

    v438 fecha vazamento de capsula em 7 superficies galeria busca feed relacionados aplica filtro de visibilidade

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v437
-const SW_VERSION = "v437";
+// Tempnext Service Worker v438
+const SW_VERSION = "v438";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
