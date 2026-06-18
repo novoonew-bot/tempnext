43c33307f1ab5d4c68f1ecc1b6252badda916024
Josemar <novoonew@gmail.com>
2026-06-17 22:18:25 -0400

    v437 tela lugar hero cascata foto-do-momento depois mapa, remove gradiente verde vazio

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v436
-const SW_VERSION = "v436";
+// Tempnext Service Worker v437
+const SW_VERSION = "v437";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
