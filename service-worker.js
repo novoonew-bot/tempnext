f2c568a7386444b0ca8a93f17a3ddf183e06a377
Josemar <novoonew@gmail.com>
2026-06-18 19:25:43 -0400

    v439 tela lugar contadores em filete remove caixa numero cormorant

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v438
-const SW_VERSION = "v438";
+// Tempnext Service Worker v439
+const SW_VERSION = "v439";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
