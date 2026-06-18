19cf27bd4801f608453a04afa687d33c841122ab
Josemar <novoonew@gmail.com>
2026-06-17 20:58:37 -0400

    v435 remove tela login antiga preview wordmark italianno religa perfil login google

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v434
-const SW_VERSION = "v434";
+// Tempnext Service Worker v435
+const SW_VERSION = "v435";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
