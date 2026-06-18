d31558e49beefa1a479094d81630e8ecd47928d1
Josemar <novoonew@gmail.com>
2026-06-17 21:34:39 -0400

    v436 fix vazamento capsula privada tela lugar, remove prompt update nativo, dialogo sair onbrand

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v435
-const SW_VERSION = "v435";
+// Tempnext Service Worker v436
+const SW_VERSION = "v436";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
