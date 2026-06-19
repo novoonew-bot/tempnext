835b6b6f5bd4199d4107268c883dbdeb54b2e23b
Josemar <novoonew@gmail.com>
2026-06-18 22:00:19 -0400

    v443 auto reload no controllerchange aplica atualizacao sozinho no pwa desktop

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v442
-const SW_VERSION = "v442";
+// Tempnext Service Worker v443
+const SW_VERSION = "v443";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
