f0a0014c86759c7c79a17c121f4ae99399d5d3b9
Josemar <novoonew@gmail.com>
2026-06-18 21:48:21 -0400

    v441 fix tela lugar mapa hero isolation traz de volta voltar e kebab e capsula lacrada esconde foto com bloco selado

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v440
-const SW_VERSION = "v440";
+// Tempnext Service Worker v441
+const SW_VERSION = "v441";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
