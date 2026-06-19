cb663c1b3897dc58f2374238e370da01b2bceda2
Josemar <novoonew@gmail.com>
2026-06-18 21:15:30 -0400

    v440 tela lugar registros hibrido linha do tempo narrativa nos 3 primeiros depois grid por ano capsula lacrada com misterio e countdown

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v439
-const SW_VERSION = "v439";
+// Tempnext Service Worker v440
+const SW_VERSION = "v440";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
