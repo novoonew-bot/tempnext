5f4eeb66e4902c99e028d467b5e69ebb93fa21a6
Josemar <novoonew@gmail.com>
2026-06-20 18:37:46 -0400

    v444 wordmark Sair do TempnexT com T final maiusculo azul nos dois dialogos

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v443
-const SW_VERSION = "v443";
+// Tempnext Service Worker v444
+const SW_VERSION = "v444";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
