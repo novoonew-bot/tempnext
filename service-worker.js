3b82df68a8baa279689e521d0eac38366002cc2d
Josemar <novoonew@gmail.com>
2026-06-21 09:39:17 -0400

    v448 home so lugar e foto viram card grande pessoa e capsula viram linha pequena

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v447
-const SW_VERSION = "v447";
+// Tempnext Service Worker v448
+const SW_VERSION = "v448";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
