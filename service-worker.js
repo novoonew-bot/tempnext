9278d46bc7a1b71bdfc071b4cb2ccc0fed8c9fda
Josemar <novoonew@gmail.com>
2026-06-21 16:11:24 -0400

    v460 S01 busca CEP cruza cidade resolvida + S05 preserva busca

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v459
-const SW_VERSION = "v459";
+// Tempnext Service Worker v460
+const SW_VERSION = "v460";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
