50eeb0f853779efc736e411416011c5230d4ef28
Josemar <novoonew@gmail.com>
2026-06-20 20:08:33 -0400

    v445 T final do wordmark Sair com anel azul em volta nos dois dialogos

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v444
-const SW_VERSION = "v444";
+// Tempnext Service Worker v445
+const SW_VERSION = "v445";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
