22f24ac944c984abf717e969b749d357e3151921
Josemar <novoonew@gmail.com>
2026-06-21 10:59:44 -0400

    v453 busca de cidade em texto livre com filtro robusto dedupe e ranking por importancia prioriza municipio

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v452
-const SW_VERSION = "v452";
+// Tempnext Service Worker v453
+const SW_VERSION = "v453";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
