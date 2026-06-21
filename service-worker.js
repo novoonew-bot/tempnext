d071a935bcade8026cb362de80796e97e8555ed6
Josemar <novoonew@gmail.com>
2026-06-21 10:07:20 -0400

    v449 fix MapaLeaflet container already initialized cleanup na branch sem pontos e reset defensivo do container

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v448
-const SW_VERSION = "v448";
+// Tempnext Service Worker v449
+const SW_VERSION = "v449";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
