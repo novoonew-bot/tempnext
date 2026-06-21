7b707c8d6f66d9f0bef7026d4b6b1e80ba648b55
Josemar <novoonew@gmail.com>
2026-06-21 10:53:56 -0400

    v452 dbRead com storageKey distinto elimina aviso multiple GoTrueClient instances

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v451
-const SW_VERSION = "v451";
+// Tempnext Service Worker v452
+const SW_VERSION = "v452";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
