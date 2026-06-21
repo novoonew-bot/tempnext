5b24269c6579e1c8174b8c35ff33d2f1f33be369
Josemar <novoonew@gmail.com>
2026-06-21 19:30:24 -0400

    v463 descobrir card de pessoa com selo de motivo colorido

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v462
-const SW_VERSION = "v462";
+// Tempnext Service Worker v463
+const SW_VERSION = "v463";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
