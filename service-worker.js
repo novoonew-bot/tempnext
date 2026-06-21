3e026c40e47cfc36493a5ef95939cba97c60f9f9
Josemar <novoonew@gmail.com>
2026-06-21 11:55:48 -0400

    v455 fix preview e rodape do momento refletem modo antes ano e rotulo em vez de agora fixo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v454
-const SW_VERSION = "v454";
+// Tempnext Service Worker v455
+const SW_VERSION = "v455";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
