e919266f8aa2ef7c08abd8a967f0ff4d6c59db78
Josemar <novoonew@gmail.com>
2026-06-21 21:45:17 -0400

    v464 login pkce + redirect origin (token fora da url)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v463
-const SW_VERSION = "v463";
+// Tempnext Service Worker v464
+const SW_VERSION = "v464";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
