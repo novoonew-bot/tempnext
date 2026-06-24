56c03c56974c1a069eba76bcc38592a730c2217c
Josemar <novoonew@gmail.com>
2026-06-23 23:29:23 -0400

    v471 perfil completo + heartbeat

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v470
-const SW_VERSION = "v470";
+// Tempnext Service Worker v471
+const SW_VERSION = "v471";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
