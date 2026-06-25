4789900db58b171004f991250a8c4f09af2af059
Josemar <novoonew@gmail.com>
2026-06-25 19:28:03 -0400

    v475 loading state nos capitulos deep-link

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v474
-const SW_VERSION = "v474";
+// Tempnext Service Worker v475
+const SW_VERSION = "v475";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
