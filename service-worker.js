f23241733a136ed92c24bdfbaad61707b79edcb9
Josemar <novoonew@gmail.com>
2026-06-25 21:29:43 -0400

    v477 back from create forms returns to origin not home

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v476
-const SW_VERSION = "v476";
+// Tempnext Service Worker v477
+const SW_VERSION = "v477";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
