67ce37524140fca51c692c50bb316a8a522c6d31
Josemar <novoonew@gmail.com>
2026-06-26 19:26:04 -0400

    v481 tap to open shared momento capsula perfil cards from chat

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v480
-const SW_VERSION = "v480";
+// Tempnext Service Worker v481
+const SW_VERSION = "v481";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
