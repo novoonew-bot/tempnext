77c7a1a2072e70344c8fb7deeaadc16768a51ff7
Josemar <novoonew@gmail.com>
2026-06-25 23:30:43 -0400

    v479 chat attach sheet share momento capsula perfil via meta

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v478
-const SW_VERSION = "v478";
+// Tempnext Service Worker v479
+const SW_VERSION = "v479";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
