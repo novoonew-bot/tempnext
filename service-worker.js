082c63c07f8efb87ebd101db993b0a651c0cfbf0
Josemar <novoonew@gmail.com>
2026-06-26 21:04:08 -0400

    v483 realtime messages subscription for live chat

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v482
-const SW_VERSION = "v482";
+// Tempnext Service Worker v483
+const SW_VERSION = "v483";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
