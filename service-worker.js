0497a02d3a5e0971b19efe8b324d56d65fb7ebba
Josemar <novoonew@gmail.com>
2026-06-26 22:23:45 -0400

    v485 persist audio to chat-media and surface media upload errors

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v484
-const SW_VERSION = "v484";
+// Tempnext Service Worker v485
+const SW_VERSION = "v485";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
