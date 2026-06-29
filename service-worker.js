2332666886414098ad2c4ba9dac392a0250dc866
Josemar <novoonew@gmail.com>
2026-06-28 22:41:50 -0400

    v492 buscar redesign etapas 2-5

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Tempnext Service Worker v491
-const SW_VERSION = "v491";
+// Tempnext Service Worker v492
+const SW_VERSION = "v492";
 const CACHE_NAME = "tempnext-" + SW_VERSION + "-" + Date.now();
 const ASSETS_HOST = ["fonts.googleapis.com", "fonts.gstatic.com", "images.unsplash.com"];
 
